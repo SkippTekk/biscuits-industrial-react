4178995d59052a0caf4cf72545dcefe5d750668c
atquick <atquick@outlook.com>
2022-12-03 00:01:44 -0500

    fixing a merging issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Announcement from "./components/Announcement";
+import Footer from "./components/Footer/Footer";
+
 import Home from "./Pages/Home";
-import Ship from "./Pages/Ship";
-import Ships from "./Pages/Ships";
 import About from "./Pages/About";
 import PageNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/ships" element={<Ship />} />
-      <Route path="/ships/:id" element={<Ships />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <>
+      <Header />
+      <Navbar />
+      <Announcement />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
