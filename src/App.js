662e95a22229c893151a39a88d848d3cf878bcba
atquick <atquick@outlook.com>
2022-12-02 23:40:45 -0500

    Removed left, right, middle section components, merged all to Home.js. Created a DynamicMeta component to change meta as we see fit, specially for pretty meta data for discord/slack, need to work on ship manu dropdown, have example data.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Ship from './Pages/Ship';
@@ -14,6 +15,32 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path='*' element={<PageNotFound />} />
    </Routes>
+=======
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Announcement from "./components/Announcement";
+import Footer from "./components/Footer/Footer";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import PageNotFound from "./Pages/PageNotFound";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Announcement />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </>
+>>>>>>> Stashed changes
   );
 }
 
